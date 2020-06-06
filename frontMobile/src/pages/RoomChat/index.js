41e59edcb995ff515ebed99c901318f380032e7e
Jefferson silva brito <46037459+jeffersontk@users.noreply.github.com>
2020-06-05 22:39:52 -0300

    adicionando navegação

--- frontMobile/src/pages/RoomChat/index.js
+++ frontMobile/src/pages/RoomChat/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
-import { View, Text, Image, TextInput, KeyboardAvoidingView } from 'react-native'
+import { View, Text, Image, TextInput, KeyboardAvoidingView, TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 import styles from './styles'
-
 import logoIconWhite from '../../assets/img/logoIconWhite.png'
 import btnArrowLeft from '../../assets/img/btnArrowLeft.png'
 import btnSend from '../../assets/img/btnSend.png'
 import btnAddPhoto from '../../assets/img/btnAddPhoto.png'
 
 export default function RoomChat() {
+    const navigation = useNavigation()
+    function navigationToRoom() {
+        navigation.navigate('Room')
+    }
     return (
         <KeyboardAvoidingView style={styles.container}>
             <View style={styles.header}>
-                <Image style={{ width: 30, height: 30 }} source={btnArrowLeft} />
-                <Text style={styles.titlePage}>Ponto 1</Text>
+                <TouchableOpacity onPress={navigationToRoom}>
+                    <Image style={{ width: 30, height: 30 }} source={btnArrowLeft} />
+                </TouchableOpacity>
+                <Text style={styles.titlePage}>Ponto</Text>
                 <Image style={{ width: 35, height: 35 }} source={logoIconWhite} />
             </View>
             <View style={styles.line}></View>
